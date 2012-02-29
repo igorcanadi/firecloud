0568c8f348e2f29029bd08a9be2382c2652964b0
Igor Canadi <igor.canadi@gmail.com>
2012-02-28 19:43:47 -0600

    Add timeout to UDP recieve + implement new version of the protocol (with unique_ids)

--- client/lib739kv.c
+++ client/lib739kv.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -12,11 +13,13 @@
 #define MAX_SERVERS 4
 #define MAX_VALUE_LEN 2048
 #define MAX_RETURN_LEN 3000
+#define MICRO_TIMEOUT 3000
 
 char *servers[MAX_SERVERS];
 char killed[MAX_SERVERS];
 int nodes_down;
 int servers_size;
+int last_unique_id;
 struct sockaddr_in server_addresses[MAX_SERVERS];
 
 // -1 on failure
@@ -84,30 +87,6 @@ void kv739_recover(char *server) {
     assert(false);
 }
 
-// -1 on failure
-// 0 on ok
-int udp_send(int node_to_send_to, char *query, char *ret, int max_ret_size) {
-    int sck, retval = 0;
-    struct sockaddr_in *server = &server_addresses[node_to_send_to];
-
-    // open the socket
-    sck = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sck < 0) {
-        retval = -1;
-    }
-
-    if (retval != -1 && sendto(sck, query, strlen(query), 0, (struct sockaddr *)server, sizeof(*server)) != strlen(query)) {
-        retval = -1;
-    }
-
-    if (retval != -1 && recvfrom(sck, ret, max_ret_size, 0, NULL, NULL) < 0) {
-        retval = -1;
-    }
-
-    close(sck);
-    return retval;
-}
-
 int choose_random_node() {
     int ret;
 
@@ -119,16 +98,80 @@ int choose_random_node() {
     return ret;
 }
 
-int send_query_string(char *query, char *value) {
-    int node_to_send_to;
-    char *return_buffer;
+// -1 on error
+// 0 on timeout
+// number of bytes on success
+int get_me_the_data_with_timeout(int sck, char *ret, int max_ret_size, int timeout_usec) {
+    fd_set read_fset;
+    struct timeval timeout;
     int retval = 0;
 
-    node_to_send_to = choose_random_node();
-    if (node_to_send_to == -1) {
+    FD_ZERO(&read_fset);
+    FD_SET(sck, &read_fset);
+    timeout.tv_sec = 0;
+    timeout.tv_usec = timeout_usec;
+
+    retval = select(sck + 1, &read_fset, NULL, NULL, &timeout);
+
+    if (retval > 0 && (retval = recvfrom(sck, ret, max_ret_size, 0, NULL, NULL)) < 0) {
         retval = -1;
-        goto out;
     }
+
+    return retval;
+}
+
+// * OK [unique_id] [value]
+// return 0 if i got the correct request_id
+// return 1 if request_id is differnet
+// return -1 if error in parsing
+int parse_ok_reply(char *reply, char *value, int request_id) {
+    int i, id_got = 0, state = 0, i_val = 0;
+
+    if (strncmp("OK", reply, 2) != 0) {
+        return -1;
+    }
+        
+    for (i = 2; reply[i]; ++i) {
+        if (reply[i] == '[') {
+            if (state != 0 || state == 2) {
+                return -1;
+            }
+            ++state;
+        } else if (reply[i] == ']') {
+            if (state == 1) {
+                if (id_got != request_id) {
+                    return 1;
+                }
+                ++state;
+            } else if (state == 3) {
+                ++state;
+                break;
+            } else {
+                return -1;
+            }
+        } else if (state == 1) {
+            if (reply[i] < '0' || reply[i] > '9') {
+                return -1;
+            }
+            id_got = id_got * 10 + reply[i] - '0';
+        } else if (state == 3) {
+            value[i_val++] = reply[i];
+        }
+    }
+    
+    if (id_got == request_id && state == 4) {
+        // parsing good
+        return 0;
+    }
+    return -1;
+}
+
+int send_query_string(char *query, char *value, int request_id) {
+    int node_to_send_to, i;
+    char *return_buffer;
+    int retval = 0;
+    int sck;
+    struct sockaddr_in *server;
 
     return_buffer = (char *)malloc(sizeof(char) * (MAX_RETURN_LEN));
     if (return_buffer == NULL) {
@@ -136,25 +179,43 @@ int send_query_string(char *query, char *value) {
         goto out;
     }
 
-    if (udp_send(node_to_send_to, query, return_buffer, MAX_RETURN_LEN) == -1) {
+    // open the socket
+    sck = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sck < 0) {
         retval = -1;
-        goto out;
+        goto closesocket;
     }
 
-    // return strings:
-    // * OK [value]
-    // * EM []
-
-    // no key
-    if (strncmp("EM", return_buffer, 2) == 0) {
-        retval = 1;
-    } else if (strncmp("OK", return_buffer, 2) == 0) {
-        strncpy(value, return_buffer + 4, strlen(return_buffer) - 5);
-    } else {
-        // unrecognized return string
-        assert(false);
+    node_to_send_to = choose_random_node();
+    if (node_to_send_to == -1) {
+        retval = -1;
+        goto closesocket;
+    }
+    server = &server_addresses[node_to_send_to];
+    if (sendto(sck, query, strlen(query), 0, (struct sockaddr *)server, sizeof(*server)) != strlen(query)) {
+        retval = -1;
+        goto closesocket;
     }
 
+    do {
+        retval = get_me_the_data_with_timeout(sck, return_buffer, MAX_RETURN_LEN, MICRO_TIMEOUT);
+
+        if (retval == -1) {
+            // error
+            goto closesocket;
+        } else if (retval == 0) {
+            // timeout TODO try another server
+            goto closesocket;
+        }
+
+        if (parse_ok_reply(return_buffer, value, request_id) == 0) {
+            // i got ok response
+            break;
+        }
+    } while (true);
+
+closesocket:
+    close(sck);
 out:
     free(return_buffer);
     return retval;
@@ -171,9 +232,9 @@ int kv739_get(char *key, char *value) {
     if (query_string == NULL) {
         return -1;
     }
-    sprintf(query_string, "GET [%s]", key);
+    sprintf(query_string, "GET [%s] [%d]", key, ++last_unique_id);
 
-    retval = send_query_string(query_string, value);
+    retval = send_query_string(query_string, value, last_unique_id);
 
     free(query_string);
     return retval;
@@ -190,9 +251,9 @@ int kv739_put(char *key, char *value, char *old_value) {
     if (query_string == NULL) {
         return -1;
     }
-    sprintf(query_string, "PUT [%s] [%s]", key, value);
+    sprintf(query_string, "PUT [%s] [%s] [%d]", key, value, ++last_unique_id);
 
-    retval = send_query_string(query_string, old_value);
+    retval = send_query_string(query_string, old_value, last_unique_id);
 
     free(query_string);
     return retval;
