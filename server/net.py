b2c7ef6ffcdae15d3367405e6615ac5a6655e24e
David Capel <wot.narg@gmail.com>
2012-02-28 20:07:49 -0600

    network

--- server/net.py
+++ server/net.py
@@ -1,5 +1,9 @@
+import cPickle as pickle
+import socket
+
+
 def Network(object):
-  def __init__(self, addrs, me, master, flood):
+  def __init__(self, addrs, me, master, dispatch):
     self.master = master
     self.me = me
 
@@ -11,24 +15,25 @@ def Network(object):
     self.s = socket.socket(AF_INET, socket.SOCK_DGRAM)
     self.s.bind(me)
 
+  def flood_stream(s):
+    pass
 
-  def flood(orig, pkt):
-    c = pkt.cooked()
+  def __flood(self, orig, data):
     for a in filter(lambda x: x != orig, self.addrs):
-      self.s.sendto(c, a)
+      self.s.sendto(data, a)
 
-  def floodAck(pkt):
-    ack = Pkt(pkt.pkt)
-    ack.make_ack()
-    c = ack.cooked()
-    for a in self.addrs:
-      self.s.sendto(pkt, a)
+  def flood(self, pkt):
+    pkt.orig = self.me
+    pkt.is_master = self.master
+    data = pickle.dumps(pkt)
+
+    self.__flood(None, data)
 
   def poll(self):
     while True:
       (data, addr) = self.s.recvfrom()
-      pkt = Pkt(data)
-      flood.recv(addr, pkt)
-
-  def i_am_master(self):
-    return self.master
+      pkt = pickle.loads(data)
+      dispatch.recv(pkt)
+      
+      if not self.has_seen(pkt):
+        self.__flood(addr, data)
