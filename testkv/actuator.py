d4ff9b4120a0e2f7a59dcd04f225ade40e66a4c6
Igor Canadi <igor.canadi@gmail.com>
2012-03-06 19:32:29 -0600

    Implemented actuattor.py. did not test

--- testkv/actuator.py
+++ testkv/actuator.py
@@ -4,7 +4,10 @@ Actuates commands in the test system
 
 The 'host' variable is always some string that you can shove
 into the sock's connect method to get get a connection.
+
+REQUIREMENT: id_rsa in current directory
 """
+import subprocess
 
 
 def partition(host1, port1, host2, port2):
@@ -20,8 +23,15 @@ def partition(host1, port1, host2, port2):
   @param host2: a string that is either the IP address or something that
   DNS can resolve to an IP adddress
   @type port2: int
+  @returns 0 on OK, non-0 on PANIC
   """
-  pass
+  command = "ssh -i id_rsa user739@%s \"sudo iptables -A INPUT -s %s -j REJECT\"" % (host1, host2)
+  retval = subprocess.call(command, shell=True)
+  if retval != 0:
+      return retval
+  command = "ssh -i id_rsa user739@%s \"sudo iptables -A INPUT -s %s -j REJECT\"" % (host2, host1)
+  return subprocess.call(command, shell=True)
+
 
 
 def partition_heal(host1, port1, host2, port2):
@@ -36,10 +46,17 @@ def partition_heal(host1, port1, host2, port2):
   @param host2: a string that is either the IP address or something that
   DNS can resolve to an IP adddress
   @type port2: int
+  @returns 0 on OK, non-0 on PANIC
   """
-  pass
+  command = "ssh -i id_rsa user739@%s \"sudo iptables -D INPUT -s %s -j REJECT\"" % (host1, host2)
+  retval = subprocess.call(command, shell=True)
+  if retval != 0:
+      return retval
+  command = "ssh -i id_rsa user739@%s \"sudo iptables -D INPUT -s %s -j REJECT\"" % (host2, host1)
+  return subprocess.call(command, shell=True)
 
 
+# kill `lsof | grep 10000 | awk '{print $2}'`
 def take_server_down(host, port):
   """ Takes the server down / kills the process / kills the VM.
   Implement this only if Swift says we need it
@@ -47,16 +64,22 @@ def take_server_down(host, port):
   @param host: a string that is either the IP address or something that
   DNS can resolve to an IP adddress
   @type port: int
+  @returns 0 on OK, non-0 on PANIC
   """
-  pass
+  command = "ssh -i id_rsa user739@%s \"kill `lsof | grep %d | awk '{print $2}'`\"" % (host, port)
+  return subprocess.call(command, shell=True)
 
 
-def bring_server_up(host, port):
+# requirement: main.py in the home directory on the server
+def bring_server_up(host, port, hosts, index, ismaster):
   """ Undo-what ever take_server_down does, so that the server works again
   Implement this only if Swift says we need it
   @type host: str
   @param host: a string that is either the IP address or something that
   DNS can resolve to an IP adddress
   @type port: int
+  @param hosts list of strs in format 'host:port'
+  @returns 0 on OK, non-0 on PANIC
   """
-  pass
+  command = "ssh -i id_rsa user739@%s \"python ~/main.py %d %d %s\"" % (index, ismaster, " ".join(hosts))
+  return subprocess.call(command, shell=True)
