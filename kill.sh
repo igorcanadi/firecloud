bbcefe83da15ba6ffca5ecf391b53bc6fc34fb82
Igor Canadi <igor.canadi@gmail.com>
2012-03-13 15:30:43 -0500

    Added parameters to scripts kill and launch

--- kill.sh
+++ kill.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 source config.sh
 
-for i in 0 1 2 3; do
+if [ $# -eq 0 ]; then
+    # no input, kill all of them
+    who_to_kill=(0 1 2 3)
+else 
+    who_to_kill=($1)
+fi
+
+for i in ${who_to_kill[@]}; do
     echo "Killing " ${servers[$i]}
-    ssh -i keys/id_rsa user739@localhost -p ${ports[$i]} "kill \`lsof | grep ${port} | awk '{print \$2}'\`"
+    ssh -i keys/id_rsa user739@localhost -p ${ports[$i]} "kill \`lsof | grep ${port} | awk '{print \$2}'\` 2> /dev/null"
 done
--- launch.sh
+++ launch.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 source config.sh
 
-master=1
-for i in 0 1 2 3; do
+if [ $# -lt 2 ]; then
+    # no input, start all of them
+    who_to_start=(0 1 2 3)
+    master=1
+else 
+    # usage: [index of the server] [is_master]
+    who_to_start=($1)
+    master=($2)
+fi
+
+for i in ${who_to_start[@]}; do
     echo "Launching " ${servers[$i]}
-    ssh -i keys/id_rsa -p ${ports[$i]} user739@localhost "python server/main.py $i $master ${servers[0]}:$port ${servers[1]}:$port ${servers[2]}:$port ${servers[3]}:$port" &
+    ssh -i keys/id_rsa -p ${ports[$i]} user739@localhost "python server/main.py $i $master ${servers[0]}:$port ${servers[1]}:$port ${servers[2]}:$port ${servers[3]}:$port 2> /dev/null" &
     master=0
 done
