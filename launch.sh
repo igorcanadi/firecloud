f8efc0e0b6867438e0147144bada0f81c6f0a12e
Igor Canadi <igor.canadi@gmail.com>
2012-03-19 22:26:31 -0500

    Changing scripts so Victor could love them

--- launch.sh
+++ launch.sh
@@ -5,11 +5,11 @@ if [ $# -lt 2 ]; then
     # no input, start all of them
     who_to_start=(0 1 2 3)
 else 
-    # usage: [index of the server] [is_master]
+    # usage: [index of the server]
     who_to_start=($1)
 fi
 
 for i in ${who_to_start[@]}; do
     echo "Launching " ${servers[$i]}
-    ssh -i keys/id_rsa -p 22 user739@${servers[$i]} "python server/main.py $i ${servers[0]}:$port ${servers[1]}:$port ${servers[2]}:$port ${servers[3]}:$port" &
+    ssh -i keys/id_rsa user739@${servers[$i]} "python server/main.py $i ${servers[0]}:$port ${servers[1]}:$port ${servers[2]}:$port ${servers[3]}:$port" &
 done
