4a8779b67ecdf1dccc4d7fdc958b5c3712382089
Victor Bittorf <bittorf@bitfort.cs.wisc.edu>
2012-03-20 17:04:31 -0500

    fix indent in perf.c

--- benchmarks/perf.c
+++ benchmarks/perf.c
@@ -9,12 +9,12 @@ int main() {
     char buf[1000];
 
     for (i = 0; i < 5; ++i) {
-        s[i] = (char *)malloc(100);
+      s[i] = (char *)malloc(100);
     }
     sprintf(s[0], "192.168.56.101:8808");
-        sprintf(s[1], "192.168.56.102:8808");
-            sprintf(s[2], "192.168.56.103:8808");
-            sprintf(s[3], "192.168.56.104:8808");
+    sprintf(s[1], "192.168.56.102:8808");
+    sprintf(s[2], "192.168.56.103:8808");
+    sprintf(s[3], "192.168.56.104:8808");
 
     sprintf(s[4], "");
 
@@ -24,21 +24,21 @@ int main() {
     sprintf(t3, "yay");
     sprintf(t4, "wohoooooo");
 
-int er;
+    int er = 0;
 
     for (i = 0; i < 300; ++i) {
-        t1[1] = rand()%3 + '0';
-        t1[2] = rand()%10 + '0';
-        t2[1] = rand()%3 + '0';
-        t2[2] = rand()%10 + '0';
-        char *a = (rand()%2) ? t3 : t4;
-        int r = kv739_put(t1, a, buf);
-if (r == -1) ++er;
-        r = kv739_get(t2, buf);
-if (r == -1) ++er;
+      t1[1] = rand()%3 + '0';
+      t1[2] = rand()%10 + '0';
+      t2[1] = rand()%3 + '0';
+      t2[2] = rand()%10 + '0';
+      char *a = (rand()%2) ? t3 : t4;
+      int r = kv739_put(t1, a, buf);
+      if (r == -1) ++er;
+      r = kv739_get(t2, buf);
+      if (r == -1) ++er;
     }
 
-printf("errors: %d/600\n", er);
+    printf("errors: %d/600\n", er);
 
     return 0;
 
