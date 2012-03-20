1e446e0a0ab9e43d94b4db1f5336d54b96ab792b
Igor Canadi <igor.canadi@gmail.com>
2012-03-19 21:05:07 -0500

    Fix local_launch.sh

--- local_launch.sh
+++ local_launch.sh
@@ -1,4 +1,4 @@
-python main.py 0 1 localhost:10000 localhost:10001 localhost:10002 localhost:10003 &
-python main.py 1 0 localhost:10000 localhost:10001 localhost:10002 localhost:10003 &
-python main.py 2 0 localhost:10000 localhost:10001 localhost:10002 localhost:10003 &
-python main.py 3 0 localhost:10000 localhost:10001 localhost:10002 localhost:10003 &
+python main.py 0 localhost:10000 localhost:10001 localhost:10002 localhost:10003 &
+python main.py 1 localhost:10000 localhost:10001 localhost:10002 localhost:10003 &
+python main.py 2 localhost:10000 localhost:10001 localhost:10002 localhost:10003 &
+python main.py 3 localhost:10000 localhost:10001 localhost:10002 localhost:10003 &
