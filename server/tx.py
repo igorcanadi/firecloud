61c9711cb524f8e79d5c94f888e2b5e57e57d43b
David Capel <wot.narg@gmail.com>
2012-03-19 23:13:50 -0500

    logging

--- server/tx.py
+++ server/tx.py
@@ -49,17 +49,17 @@ class Tx(object):
 
   def ack(self, entry, is_master):
     assert type(entry.key) is str
-    #print self, "acked by", entry
+    print self, "acked by", entry
     if self.entry is None or entry.ts > self.entry.ts:
       self.entry = entry
 
     self.acks += 2 if is_master else 1
 
-    #print '   @', self.acks, 'acks'
+    print '   @', self.acks, 'acks'
 
     if self.acks >= 3 and self.state == UNCOMMITED:
       self.state = ZOMBIE
-      #print '   -> Commit'
+      print '   -> Commit'
       self.commit()
 
     if self.acks == 5:
