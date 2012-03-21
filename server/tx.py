9e99f6a9e7d581be9555ff3337d7a088b305e450
David Capel <wot.narg@gmail.com>
2012-03-20 19:28:30 -0500

    logging

--- server/tx.py
+++ server/tx.py
@@ -126,6 +126,7 @@ class Tx(object):
     self.net.commit(self)
 
   def ack(self, entry, is_master):
+    log("state: " + str(self.state) + " ack " + str(entry))
     assert type(entry.key) is str
     if self.entry is None or entry.ts > self.entry.ts:
       self.entry = entry
@@ -134,6 +135,8 @@ class Tx(object):
       self.state, action = transition(self.state, master)
     else:
       self.state, action = transition(self.state, normal)
+    
+    log(str(self.state) + " action " + str(action)) 
 
     if action == COMMIT:
       self.commit()
