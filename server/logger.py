0b79fdeacbe0328d2fc28c2cee57e3be6884aea1
Igor Canadi <igor.canadi@gmail.com>
2012-03-20 19:40:43 -0500

    adding flag to disable logging

--- server/logger.py
+++ server/logger.py
@@ -2,10 +2,12 @@
 from time import time
 
 LOG_FILE = '/tmp/server.log'
+VERBOSE = 0
 
 def log(txt):
-  if not '\n' in txt:
-    txt += '\n'
-  with open(LOG_FILE, 'a') as f:
-    f.write('%s : %s' % (time(), txt))
+  if VERBOSE:
+      if not '\n' in txt:
+        txt += '\n'
+      with open(LOG_FILE, 'a') as f:
+        f.write('%s : %s' % (time(), txt))
 
