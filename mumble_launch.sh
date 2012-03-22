124f66a9c6ed3dd09fce2a6c6b4efbec64b81c66
Igor Canadi <igor.canadi@gmail.com>
2012-03-21 19:37:44 -0500

    mumble launch fix

--- mumble_launch.sh
+++ mumble_launch.sh
@@ -1,1 +1,1 @@
-python main.py $1 mumble-07.cs.wisc.edu:8808 mumble-08.cs.wisc.edu:8808 mumble-09.cs.wisc.edu:8808 mumble-10.cs.wisc.edu:8808
+python server/main.py $1 mumble-07.cs.wisc.edu:8808 mumble-08.cs.wisc.edu:8808 mumble-09.cs.wisc.edu:8808 mumble-10.cs.wisc.edu:8808
