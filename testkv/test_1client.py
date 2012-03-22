96768e0d3c60e9d7817cd4092525cc2ba0be7692
Victor Bittorf <bittorf@bitfort.cs.wisc.edu>
2012-03-22 18:40:04 -0500

    testing changes

---
+++ testkv/test_1client.py
@@ -0,0 +1,41 @@
+
+
+from framework import Client, Server, Clock, Transcript, Network, CLOCK_RATE
+from analysis import replay_gets_into_dict, merge_traces, \
+                    eval_strict_ordering, count_errors
+
+from time import time
+
+
+from harness import create_harness
+from actuator import hard_reset
+
+hard_reset()
+
+
+harn = create_harness()
+
+NUM_CLIENTS = 4
+
+REQS = 3000
+
+cli = harn.client_by_mask(0xF)
+
+CLOCK_RATE = 3
+
+for r in xrange(REQS/2):
+  cli['foo'] = r
+  cli['foo']
+
+start = time()
+harn.execute(CLOCK_RATE)
+end = time()
+
+print 'Delta time (s): ', end - start
+print 'Reqs / second: ', REQS * 1.0 / (end-start)
+
+print 'Strict Ordering: ', eval_strict_ordering(cli.ctrace)
+
+harn.print_stats()
+
+
